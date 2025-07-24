68d331ff09955b0bb1ff93c83bcdda07abb09e72
Ali Abbas Abidi <smaliabbasa890@gmail.com>
2025-07-24 06:12:54 +0500

    companies added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # array-stores-companies
+companies is stored in array. user can enter company name which is added in the end of array.
---
+++ script.js
@@ -0,0 +1,5 @@
+let companies = ["Microsoft", "Bloomberg", "Uber", "Google"]
+console.log(companies); 
+let pushcompany = prompt("enter company name : ");
+console.log(pushcompany);
+companies.push(pushcompany);
